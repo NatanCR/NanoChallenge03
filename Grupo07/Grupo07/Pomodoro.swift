eb51d8e524731b1d8483bb86311c093ec6ea0bcb
Natan Camargo Rodrigues <natanrodrigues0044@gmail.com>
2022-04-19 15:59:25 -0300

    pomodoro rodando

---
+++ Grupo07/Grupo07/Pomodoro.swift
@@ -0,0 +1,71 @@
+//
+//  Pomodoro.swift
+//  Grupo07
+//
+//  Created by Natan Rodrigues on 19/04/22.
+//
+
+import Foundation
+import UIKit
+class Pomodoro{
+    
+
+//cria as constantes pro circulo
+let foreProgressLayer = CAShapeLayer()
+let backProgressLayer = CAShapeLayer()
+let animation = CABasicAnimation(keyPath: "strokeEnd")
+
+//cria variaveis do timer
+var timer = Timer()
+var isTimerStarted = false
+var isAnimationStarted = false
+var time = 5
+var count = 0
+
+
+//funcao para calcular os segundos e minutos do timer
+func formatTime()->String{
+    let minutes = Int(time) / 60 % 60
+    let seconds = Int(time) % 60
+    return String(format:"%02i:%02i", minutes, seconds)
+}
+    
+func resetAnimation(){
+    foreProgressLayer.speed = 1.0
+    foreProgressLayer.timeOffset = 0.0
+    foreProgressLayer.beginTime = 0.0
+    foreProgressLayer.strokeEnd = 0.0
+    isAnimationStarted = false
+}
+
+func pauseAnimation(){
+    let pausedTime = foreProgressLayer.convertTime(CACurrentMediaTime(), from: nil)
+    foreProgressLayer.speed = 0.0
+    foreProgressLayer.timeOffset = pausedTime
+}
+
+func resumeAnimation(){
+    let pausedTime = foreProgressLayer.timeOffset
+    foreProgressLayer.speed = 1.0
+    foreProgressLayer.timeOffset = 0.0
+    foreProgressLayer.beginTime = 0.0
+    let timeSincePaused = foreProgressLayer.convertTime(CACurrentMediaTime(), from: nil) - pausedTime
+    foreProgressLayer.beginTime = timeSincePaused
+}
+
+func stopAnimation(){
+    foreProgressLayer.speed = 1.0
+    foreProgressLayer.timeOffset = 0.0
+    foreProgressLayer.beginTime = 0.0
+    foreProgressLayer.strokeEnd = 0.0
+    foreProgressLayer.removeAllAnimations()
+    isAnimationStarted = false
+}
+    
+}
+  
+extension Int{
+var degreesToRadians : CGFloat{
+    return CGFloat(self) * .pi / 180 //180
+}
+}
--- Grupo07/Grupo07/ViewController.swift
+++ Grupo07/Grupo07/ViewController.swift
@@ -7,10 +7,127 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class ViewController: UIViewController, CAAnimationDelegate {
+    
+    let pomodoro = Pomodoro()
 
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet var label: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        drawBackLayer()
     }
-}
+    
+    @IBAction func startButtonTapped(_ sender: Any) {
+        cancelButton.isEnabled = true
+        cancelButton.alpha = 1.0
+        cancelButton.setTitleColor(UIColor.red, for: .normal) //cor do botao cancelar
+       
+        
+        if !pomodoro.isTimerStarted{ //logica para mostrar o botao de pause apos iniciar o timer
+            drawForeLayer()
+            startResumeAnimation()
+            startTimer()
+            pomodoro.isTimerStarted = true
+            startButton.setTitle("Pause", for: .normal)
+            startButton.setTitleColor(UIColor.orange, for: .normal)
+        }else {
+            pomodoro.pauseAnimation()
+            pomodoro.timer.invalidate() //logica para mostrar o botao resume caso pause o timer
+            pomodoro.isTimerStarted = false
+            startButton.setTitle("Resume", for: .normal)
+            startButton.setTitleColor(UIColor.green, for: .normal)
+        }
+    }
 
+    //botao de cancelar
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        pomodoro.stopAnimation()
+        cancelButton.isEnabled = false //logica para mostrar o botao de start apos cancelar o timer
+        cancelButton.alpha = 0.5
+        startButton.setTitle("Start", for: .normal)
+        startButton.setTitleColor(UIColor.green, for: .normal)
+        
+        //faz o timer voltar ao inicio e contar 25 min novamente
+        pomodoro.timer.invalidate()
+        pomodoro.time = 5
+        pomodoro.isTimerStarted = false
+        timeLabel.text = "25:00"
+    
+}
+    
+    @objc func updateTimer(){
+        if pomodoro.time<1{
+            pomodoro.count = pomodoro.count + 1
+            label.text = "\(pomodoro.count)"
+            cancelButton.isEnabled = false
+            cancelButton.alpha = 0.5
+            startButton.setTitle("Start", for: .normal)
+            startButton.setTitleColor(UIColor.green, for: .normal)
+            pomodoro.timer.invalidate()
+            pomodoro.time = 5
+            pomodoro.isTimerStarted = false
+            timeLabel.text = "25:00"
+        }else{
+            pomodoro.time -= 1
+            timeLabel.text = pomodoro.formatTime()
+        }
+    }
+    
+    func startTimer(){
+        pomodoro.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(updateTimer)), userInfo: nil, repeats: true)
+    }
+    
+    //logica para continuar de onde foi pausado
+    func startResumeAnimation(){
+        if !pomodoro.isAnimationStarted{
+            startAnimation()
+        }else{
+            pomodoro.resumeAnimation()
+        }
+    }
+
+    //quando a animação estiver concluída, a barra de progresso circular vermelha deve ser redefinida
+    //funcao para fazer a animacao rodar
+    func startAnimation(){
+        pomodoro.resetAnimation()
+        pomodoro.foreProgressLayer.strokeEnd = 0.0
+        pomodoro.animation.keyPath = "strokeEnd"
+        pomodoro.animation.fromValue = 0
+        pomodoro.animation.toValue = 1
+        pomodoro.animation.duration = 5
+        pomodoro.animation.delegate = self
+        pomodoro.animation.isRemovedOnCompletion = false
+        pomodoro.animation.isAdditive = true
+        pomodoro.animation.fillMode = CAMediaTimingFillMode.forwards
+        pomodoro.foreProgressLayer.add(pomodoro.animation, forKey: "strokeEnd")
+        pomodoro.isAnimationStarted = true
+    }
+    
+    internal func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        pomodoro.stopAnimation()
+    }
+    
+    //cria o circulo e personaliza //background layer
+    func drawBackLayer(){
+        pomodoro.backProgressLayer.path = UIBezierPath(arcCenter: CGPoint(x:view.frame.midX, y:view.frame.midY), radius: 100, startAngle: -90.degreesToRadians, endAngle: 270.degreesToRadians, clockwise: true).cgPath
+        pomodoro.backProgressLayer.strokeColor = UIColor.white.cgColor
+        pomodoro.backProgressLayer.fillColor = UIColor.clear.cgColor
+        pomodoro.backProgressLayer.lineWidth = 15
+            view.layer.addSublayer(pomodoro.backProgressLayer)
+    }
+    
+    //funcao cria o circulo que sobrepoe o primeiro
+    //fore layer which is the animated red one
+    func drawForeLayer(){
+        pomodoro.foreProgressLayer.path = UIBezierPath(arcCenter: CGPoint(x:view.frame.midX, y:view.frame.midY), radius: 100, startAngle: -90.degreesToRadians, endAngle: 270.degreesToRadians, clockwise: true).cgPath
+        pomodoro.foreProgressLayer.strokeColor = UIColor.red.cgColor
+        pomodoro.foreProgressLayer.fillColor = UIColor.clear.cgColor
+        pomodoro.foreProgressLayer.lineWidth = 15
+            view.layer.addSublayer(pomodoro.foreProgressLayer)
+    }
+    }
