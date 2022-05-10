cdef66306c908d141be715f2fae57379d0dd8077
Sarah <sarahfrontdev@gmail.com>
2022-05-10 17:42:14 -0300

    Custom screen

---
+++ Grupo07/Grupo07/Controllers/SimuladosViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SimuladosViewController.swift
+//  Grupo07
+//
+//  Created by Sarah dos Santos Silva on 10/05/22.
+//
+
+import UIKit
+
+class SimuladosViewController: UIViewController {
+
+    @IBOutlet weak var simuladosView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
